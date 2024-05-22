856ea864879edfc80c024275f307d41bd27f573e
evgenykovich <jenya.issakovic@gmail.com>
2024-05-22 14:48:37 +0300

    remove depricated config

--- app/api/detect/route.ts
+++ app/api/detect/route.ts
@@ -11,33 +11,11 @@ export const POST = async (request: any) => {
     })
   }
 
-  // const analysisResult = await analyze(image, items)
   const analysisResult = await test(image, items)
-
-  console.log('analysisResult', analysisResult)
-
-  // if (!analysisResult) {
-  //   return new NextResponse(JSON.stringify({ error: 'Analysis failed' }), {
-  //     status: 500,
-  //   })
-  // }
-
-  // const detectedItems = analysisResult.objectsOnImage
-  //   .split(',')
-  //   .map((item: string) => item.trim())
-  // const missingItems = items.filter(
-  //   (item: any) => !detectedItems.includes(item)
-  // )
 
   const detectedItems = analysisResult.message.content
 
   return new NextResponse(JSON.stringify({ detectedItems }), {
     status: 200,
   })
-}
-
-export const config = {
-  api: {
-    bodyParser: true,
-  },
 }
